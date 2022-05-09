d627b2f45edb2580b02d02a6cd920953f35fa5b3
Ezequiel <ezequieljsosa@gmail.com>
2022-05-09 01:35:15 -0300

    variables de entorno para la config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mqtt-rest-ahk
+
+Variables de entorno de ejemplo:
+urlServidor:  https://web-ahk.herokuapp.com/termometro/32
+topic: temperatura
+brokerUrl: tcp://node.myqtthub.com:1883
+usuario: eze
+password: ..
--- src/main/java/ar/edu/ahk/Server.java
+++ src/main/java/ar/edu/ahk/Server.java
@@ -9,42 +9,45 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public class Server implements MqttCallback {
-   
+
     private String tempActual = "?";
-    private MqttClient client ;
-    public MqttClient getClient(){
-    return client;
+    private MqttClient client;
+
+    public MqttClient getClient() {
+        return client;
     }
 
-    public String getTempActual(){
-    	return tempActual;
+    public String getTempActual() {
+        return tempActual;
     }
-    public void setTempActual(String tempActual){
-     this.tempActual = tempActual;
+
+    public void setTempActual(String tempActual) {
+        this.tempActual = tempActual;
     }
 
     public static void main(String[] args) {
-	Server mqttServ = new Server();
+        Server mqttServ = new Server();
         Javalin app = Javalin.create().start(getHerokuAssignedPort());
         app.get("/", ctx -> ctx.result("Test AHK MQTT - temometro: " + mqttServ.getTempActual()));
         app.get("/termometro/:temp", ctx -> {
-		int qos             = 2;
-		String temp = ctx.pathParam("temp") ;
-		MqttMessage message = new MqttMessage(temp.getBytes());
-                message.setQos(qos);
-                mqttServ.getClient().publish("temp", message);
-                mqttServ.setTempActual(temp);
-		ctx.result("temometro actualizado: " + mqttServ.getTempActual());
-	}
-	);
-	app.get("/test",ctx -> {
-	
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-                HttpGet request = new HttpGet("https://web-ahk.herokuapp.com/termometro/32");
-                System.out.println(httpClient.execute(request).getEntity().toString());
-                
-	});
-	mqttServ.subscribe(System.getenv("MQTTClient"));
+            // Deberia ser un POST, pero para que sea mas facil probar pusimos un GET
+                    int qos = 2;
+                    String temp = ctx.pathParam("temp");
+                    MqttMessage message = new MqttMessage(temp.getBytes());
+                    message.setQos(qos);
+                    mqttServ.getClient().publish("temp", message);
+                    mqttServ.setTempActual(temp);
+                    ctx.result("temometro actualizado: " + mqttServ.getTempActual());
+                }
+        );
+//        app.get("/test", ctx -> {
+//
+//            CloseableHttpClient httpClient = HttpClients.createDefault();
+//            HttpGet request = new HttpGet(System.getenv("urlServidor"));
+//            System.out.println(httpClient.execute(request).getEntity().toString());
+//
+//        });
+        mqttServ.subscribe(System.getenv("clientID"));
     }
 
     private static int getHerokuAssignedPort() {
@@ -55,28 +58,30 @@ public class Server implements MqttCallback {
         return 7000;
     }
 
-    private static final String brokerUrl ="tcp://node02.myqtthub.com:1883";
 
-    /** The topic. */
-    private static final String topic = "temp";
+
+    /**
+     * The topic.
+     */
+
 
     public void subscribe(String clientId) {
         //    logger file name and pattern to log
         MemoryPersistence persistence = new MemoryPersistence();
-        try
-        {
+        try {
+            String brokerUrl =System.getenv("brokerUrl");
             this.client = new MqttClient(brokerUrl, clientId, persistence);
             System.out.println("Starting: " + clientId);
             MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setUserName("eze");
-            connOpts.setPassword("123".toCharArray());
+            connOpts.setUserName(System.getenv("usuario"));
+            connOpts.setPassword(System.getenv("password").toCharArray());
             connOpts.setCleanSession(true);
             System.out.println("checking");
             System.out.println("Mqtt Connecting to broker: " + brokerUrl);
             client.connect(connOpts);
             System.out.println("Mqtt Connected");
             client.setCallback(this);
-            client.subscribe(topic);
+            client.subscribe(System.getenv("topic"));
             System.out.println("Subscribed");
             System.out.println("Listening");
 
@@ -84,17 +89,20 @@ public class Server implements MqttCallback {
             System.out.println(me);
         }
     }
-    //Called when the client lost the connection to the broker
+
+    //Que muestra cuando perdes la conexion con la mensajeria mqtt
     public void connectionLost(Throwable arg0) {
         System.out.println(arg0);
     }
-    //Called when a outgoing publish is complete
+
+    // se llama cuando enviaste un mensaje correctamente
     public void deliveryComplete(IMqttDeliveryToken arg0) {
     }
+
     public void messageArrived(String topic, MqttMessage message) {
-	this.tempActual = message.toString();
+        this.tempActual = message.toString();
         System.out.println("Topic:" + topic);
-        System.out.println("Message: " +message.toString());
+        System.out.println("Message: " + message.toString());
     }
 
 }
